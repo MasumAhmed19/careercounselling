ff51b498c0d9cb0a6a068c175b2cdc24b0e94d8c
MasumAhmed19 <masum.ahmed1328@gmail.com>
2024-11-21 06:05:28 +0600

    .env setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTyNrnVmZDsY3qA2XmVNslsl8nug-UHMg",
-  authDomain: "career-counseling-33462.firebaseapp.com",
-  projectId: "career-counseling-33462",
-  storageBucket: "career-counseling-33462.firebasestorage.app",
-  messagingSenderId: "897056895222",
-  appId: "1:897056895222:web:2b84bdc875311419303cd7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
